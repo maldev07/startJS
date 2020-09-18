e24823506e95a806cbd3c4cd7e6b6e2700287c11
Vova <vov.malin@yandex.ru>
2020-09-18 19:45:35 +0300

    New changes

---
+++ JS/arrays.js
@@ -0,0 +1,33 @@
+// const arr = [1, 2, 3, 6, 8];
+
+// arr.pop();
+// arr.push(10);
+
+// for (let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]);
+// }
+// console.log(arr);
+
+// for (let value of arr) {
+//    console.log(value);
+// }
+
+// arr.forEach(function (item, i, arr) {
+//    console.log(`${i}: ${item} in Array ${arr}`);
+// });
+
+// const str = 'fgdfbgfdfdbdfbdsvdfbdfbdfbdfbdfbdfbdfbdfbdfbdfbdfbdf';
+// let arr = [];
+// arr = str.split(''); // из строки в массив
+// let str2 = arr.join('');  // из массива в строку
+// let str3 = arr.sort().join('');
+// console.log(str3); 
+
+const arr2 = [2, 42, 54, 278, 101, 23, 1, 22];
+arr2.sort(sortBy);
+console.log(arr2);
+
+
+function sortBy(a, b) {
+   return a - b;
+}
